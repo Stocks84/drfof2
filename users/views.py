b2bf883321975d3e509715eacdb26004db4bab64
Will Stocks <willstocks@Wills-MacBook-Pro.local>
2025-02-13 14:06:06 +0000

    Add views.py for users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
