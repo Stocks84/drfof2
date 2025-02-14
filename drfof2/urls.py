e6eb8b8d8b6f48071b44c2d51dc1eac7d04aed22
Will Stocks <willstocks@Wills-MacBook-Pro.local>
2025-02-14 16:24:32 +0000

    Update login token issue

--- drfof2/urls.py
+++ drfof2/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -21,7 +22,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
