181c53c9b2bca6bdaedfb117b986efac9f771f23
Will Stocks <willstocks@Wills-MacBook-Pro.local>
2025-02-14 15:58:05 +0000

    Update UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,7 +3,28 @@ from .models import CustomUser
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
+
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email',
+        fields = ['id', 'username', 'email', 'password',
                   'bio', 'location', 'favorite_drink', 'profile_picture']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Use create_user to handle password hashing
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            location=validated_data.get('location', ''),
+            favorite_drink=validated_data.get('favorite_drink', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        return user
+
