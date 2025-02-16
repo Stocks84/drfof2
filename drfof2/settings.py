c84e25e48a38a71814020ccf7f096c5ac1be1455
Will Stocks <willstocks@Wills-MacBook-Pro.local>
2025-02-16 11:15:51 +0000

    Add games app

--- drfof2/settings.py
+++ drfof2/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'corsheaders',
     'users',
+    'games',
 ]
 
 REST_FRAMEWORK = {
--- users/urls.py
+++ users/urls.py
@@ -15,5 +15,4 @@ urlpatterns = [
     path('delete-account/', UserDeleteView.as_view(), name='delete-account'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
