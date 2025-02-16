fd52838110c0945f92fe86111a45948debe59950
Will Stocks <willstocks@Wills-MacBook-Pro.local>
2025-02-16 11:22:30 +0000

    views added

--- games/views.py
+++ games/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Game
+from .serializers import GameSerializer
 
-# Create your views here.
+class GameListCreateView(generics.ListCreateAPIView):
+    queryset = Game.objects.all().order_by('-created_at')
+    serializer_class = GameSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+
+class GameDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.creator != request.user:
+            return Response({"error": "You are not allowed to edit this game."}, status=403)
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.creator != request.user:
+            return Response({"error": "You are not allowed to delete this game."}, status=403)
+        return super().destroy(request, *args, **kwargs)
