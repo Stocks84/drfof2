e872cfc1c1dcf49fc43a1657496444b4a4810fc6
Will Stocks <willstocks@Wills-MacBook-Pro.local>
2025-02-16 11:54:30 +0000

    Add Like & Comment serializers

--- games/serializers.py
+++ games/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
-from .models import Game
+from .models import Game, Like, Comment
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = '__all__'
         read_only_fields = ['creator', 'created_at', 'updated_at']
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ['id', 'user', 'game', 'created_at']
+        read_only_fields = ['user']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'game', 'text', 'created_at']
+        read_only_fields = ['user']
