aaca7ea472c781acbdbafcef4ff6370470505869
Will Stocks <willstocks@Wills-MacBook-Pro.local>
2025-02-16 12:05:44 +0000

    Update games urls.py

--- games/urls.py
+++ games/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from .views import GameListCreateView, GameDetailView
+from .views import (
+    GameListCreateView,
+    GameDetailView,
+    LikeGameView,
+    UnlikeGameView,
+    CommentGameView,
+    ViewComments,
+    DeleteCommentView
+)
 
 urlpatterns = [
-    path('games/', GameListCreateView.as_view(), name='game-list-create'),
-    path('games/<int:pk>/', GameDetailView.as_view(), name='game-detail',)
+    path('games/', GameListCreateView.as_view(), name='game-list'),
+    path('games/<int:pk>/', GameDetailView.as_view(), name='game-detail'),
+    path('games/<int:id>/like/', LikeGameView.as_view(), name='like-game'),
+    path('games/<int:id>/unlike/', UnlikeGameView.as_view(), name='unlike-game'),
+    path('games/<int:id>/comment/', CommentGameView.as_view(), name='comment-game'),
+    path('games/<int:id>/comments/', ViewComments.as_view(), name='view-comments'),
+    path('games/comments/<int:id>/', DeleteCommentView.as_view(), name='delete-comment'),
 ]
