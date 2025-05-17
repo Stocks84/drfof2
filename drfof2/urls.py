c4f5bfb0ed374147c72f212bbc05981bffd93936
Will Stocks <willstocks@Wills-MacBook-Pro.local>
2025-05-17 12:57:27 +0100

    Add CustomUserAdmin to user admin.py

--- drfof2/urls.py
+++ drfof2/urls.py
@@ -1,3 +1,4 @@
+
 """
 URL configuration for drfof2 project.
 
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_superuser', 'date_joined')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    readonly_fields = ('last_login', 'date_joined')  # Prevents modification
