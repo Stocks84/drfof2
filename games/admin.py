d4f9c9dbc1943dd19843172d35c1d4611eb4e97e
Will Stocks <willstocks@Wills-MacBook-Pro.local>
2025-05-17 13:00:08 +0100

    Add Game Like Comment class to games admin.py

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Game, Like, Comment
 
-# Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creator', 'created_at', 'updated_at')
+    search_fields = ('title', 'description', 'creator__username')
+    list_filter = ('created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'game', 'created_at')
+    search_fields = ('user__username', 'game__title')
+    list_filter = ('created_at',)
+    readonly_fields = ('created_at',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'game', 'text', 'created_at')
+    search_fields = ('user__username', 'game__title', 'text')
+    list_filter = ('created_at',)
+    readonly_fields = ('created_at',)
